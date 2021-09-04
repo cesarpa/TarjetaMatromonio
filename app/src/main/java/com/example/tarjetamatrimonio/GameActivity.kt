cd36dc65c16fe33cf4b4a858078db4ee926511b3
unknown <cesar.1023@hotmail.com>
2021-09-04 13:14:30 -0500

    TM removing unused variables

--- app/src/main/java/com/example/tarjetamatrimonio/GameActivity.kt
+++ app/src/main/java/com/example/tarjetamatrimonio/GameActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tarjetamatrimonio
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,27 +12,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class GameActivity : AppCompatActivity() {
 
-    var point = Point()
     var yPress = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        back.setOnClickListener { startActivity(Intent(this@GameActivity, MainActivity::class.java)) }
+        back.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@GameActivity,
+                    MainActivity::class.java
+                )
+            )
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        println("yPress: $yPress")
         when (event!!.action) {
-
-            MotionEvent.ACTION_DOWN ->{
+            MotionEvent.ACTION_DOWN -> {
                 yPress = event.getRawY()
             }
-            MotionEvent.ACTION_UP ->{
+            MotionEvent.ACTION_UP -> {
                 when {
-                    event.getRawY()<yPress -> player.y -= 100
-                    event.getRawY()>yPress -> player.y += 100
+                    event.getRawY() < yPress -> player.y -= 100
+                    event.getRawY() > yPress -> player.y += 100
                 }
             }
         }
