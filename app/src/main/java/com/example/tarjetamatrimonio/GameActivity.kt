f7b1e6f2c334492b30d94b4d681c2ec0b9534e87
unknown <cesar.1023@hotmail.com>
2021-09-06 23:16:04 -0500

    TM updating coin position

--- app/src/main/java/com/example/tarjetamatrimonio/GameActivity.kt
+++ app/src/main/java/com/example/tarjetamatrimonio/GameActivity.kt
@@ -1,23 +1,27 @@
 package com.example.tarjetamatrimonio
 
 import android.content.Intent
+import android.graphics.Point
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tarjetamatrimonio.controller.GameData
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class GameActivity : AppCompatActivity(), Runnable {
 
-    var yPress = 0f
+
     var thread = Thread(this)
     var playing = true;
+    var data: GameData = GameData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        thread.start()
         back.setOnClickListener {
             playing = false
             startActivity(
@@ -27,23 +31,18 @@ class GameActivity : AppCompatActivity(), Runnable {
                 )
             )
         }
-
-        val textView = TextView(this)
-        textView.textSize = 40f
-        textView.text = "text"
-
-        gameLayout.addView(textView)
+        thread.start()
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event!!.action) {
             MotionEvent.ACTION_DOWN -> {
-                yPress = event.getRawY()
+                data.yPress = event.getRawY()
             }
             MotionEvent.ACTION_UP -> {
                 when {
-                    event.getRawY() < yPress -> player.y -= 100
-                    event.getRawY() > yPress -> player.y += 100
+                    event.getRawY() < data.yPress -> player.y -= 100
+                    event.getRawY() > data.yPress -> player.y += 100
                 }
             }
         }
@@ -51,6 +50,18 @@ class GameActivity : AppCompatActivity(), Runnable {
     }
 
     override fun run() {
+        val imageView = ImageView(this)
+        imageView.setImageResource(R.drawable.coin)
+        imageView.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        imageView.getLayoutParams().height = 100
+        imageView.x = 1200f
+        val rnds = (180..780).random()
+        imageView.y = rnds.toFloat()
+        data.coinList.add(imageView)
+        gameLayout.addView(data.coinList.get(0))
         while (playing) {
             draw()
             sleep()
@@ -58,9 +69,16 @@ class GameActivity : AppCompatActivity(), Runnable {
     }
 
     private fun draw() {
-        println("running")
+        data.coinList.forEach {
+            it.x -= 10
+            if (it.x < -800) {
+                it.x = 1200f
+                val rnds = (180..780).random()
+                it.y = rnds.toFloat()
+            }
+        }
+        println("creating coins")
     }
-
     private fun sleep() {
         Thread.sleep(17)
     }
---
+++ app/src/main/java/com/example/tarjetamatrimonio/controller/GameData.kt
@@ -0,0 +1,15 @@
+package com.example.tarjetamatrimonio.controller
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.tarjetamatrimonio.R
+
+class GameData(
+    var coinList: MutableList<ImageView> = mutableListOf(),
+    var yPress: Float = 0f,
+    var maxCoins:Int = 4
+) {
+
+
+}
